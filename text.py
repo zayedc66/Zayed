45a58fa561357d25d4f731b4d8ecbb18da6716fd
zayedc66 <zayedc66@kprschools.ca>
2023-03-20 09:24:53 -0400

    almost completed just need to complete try except, and also figure out the issue with the course averages. ask ab it in class tmr

--- text.py
+++ text.py
@@ -1,55 +1,61 @@
 import os
 import time
-#import numpy as np
 
 studentData = [
-    ["John", "Doe", "Passed", 80.0, 79.0, 83.0, 72.0, 78.5],
-    ["Jane", "Doe", "Passed", 83.0, 95.0, 92.0, 94.0, 91],
-    ["George", "Python", "Failed", 37.0, 23.0, 51.0, 46.0, 39.3],
-    ["Luke", "Skywalker", "Passed", 19.0, 77.0, 80.0, 83.0, 64.8],
-    ["Teerth", "Panchal", "Failed", 5.0, 15.0, 13.0, 7.0, 10.0]
+    ["Yeast", "Dune", "Passed", 80.0, 79.0, 83.0, 72.0, 78.5],
+    ["Noel", "Noa", "Passed", 83.0, 95.0, 92.0, 94.0, 91],
+    ["Cris", "Ron", "Failed", 37.0, 23.0, 51.0, 46.0, 39.3],
+    ["Leo", "Mane", "Passed", 19.0, 77.0, 80.0, 83.0, 64.8],
+    ["Tyrone", "Ream", "Failed", 5.0, 15.0, 13.0, 7.0, 10.0]
 ]
 
-# initial variables
-menu = True
-start = False
-add = False
-list_stud = False
-Course_avg = False
-login = False
-wel_msg = "WELCOME TO STUDENT GRADES PROGRAM\n"
-start == True
+#initial variables
+options = True
+initiate = False
+addition = False
+listing = False
+course_avg = False
+welcome = "WELCOME TO STUDENT GRADES PROGRAM\n"
+initiate = True
 
 
 def main():
     # intro print statement
-    print("\nWelcome to Student Grades Program \n\n-----------------------------------\n1) Add Student to list \n2) List Students + Student Average \n3) Course Average\n4) Exit Program\n-----------------------------------")
+    print("\nWelcome to Student Grades Program \n\n-----------------------------------\n1) Add Student to list (Calculates Student Average) \n2) List Students + Student Average \n3) Course Average\n4) Exit Program\n-----------------------------------")
     user_input = int(input("Please input your desired option: "))
     check_user_input(user_input)
-
-    # if type(user_input) == int:
+    #if type(user_input) == int:
     #     num = int(user_input)
     #     check_user_input(num)
-    # else:
+    #else:
     #     print("misinpit")
     
-    
+#parentheses tell Python to execute the named function rather than just refer to the function    
 def check_user_input(user):
-    match user:
-        case 1:
-            add_student()
-        case 2:
-            list_student()
-        case 3:
-            course_average()
-        case 4:
-            quit()
-        case _:
-            try:
-                user_input = int(input("Sorry, there seems to be a misinput. Retry: ")) 
-                check_user_input(user_input)
-            except Exception as x:
-                print(x)
+        match user:
+            case 1:
+                add_student()
+            case 2:
+                list_student()
+            case 3:
+                course_average()
+            case 4:
+                quit()
+            case _:
+                try:
+                    user_input = int(input("Sorry, there seems to be a misinput. Retry: ")) 
+                    check_user_input(user_input)
+                except Exception as x:
+                    print(x)
+                '''os.system('cls')
+                print(user_input,"is not a valid menu option")
+                time. sleep(2)        
+    except Exception as e:
+                os.system('cls')
+                if not user_input == "4":
+                    print(user_input,"is not a valid menu option")
+                time. sleep(2)
+                os.system('cls') '''
 
 
 def add_student():
@@ -62,7 +68,7 @@ def add_student():
     mrks = True
     while mrks == True:    
                 os.system('cls')
-                print("Now you will enter the marks for tests 1-4")
+                print("Enter the marks for tests 1-4") 
                 try:        
                     MATH = float(input("MATH mark %: "))
                     ENGLISH = float(input("ENGLISH mark %: "))
@@ -84,14 +90,14 @@ def add_student():
                     print(f_name,l_name,passed,"with an overall mark of: ",student_avg,"%" )
                     confirm = input("Enter 'Y' to add student to database OR Enter 'N' to cancel (will return you to menu!): ").upper()
                     if confirm == "Y":
-                        confirm = True
+                        os.system('cls')
+                        studentData.append([f_name,l_name,passed,MATH,ENGLISH,HISTORY,GYM,student_avg ])
                         main()
                         os.system('cls')
                     else:
-                        confirm = False
                         print("Student was not added to list..... Returning to main menu.....")
-                        main()
                         os.system('cls')
+                        main()
 
     '''keepAdding = 1
     first = input('Plz give first')
@@ -118,6 +124,7 @@ def add_student():
         ----------------------------""")'''
 
 def list_student():
+    os.system('cls')
     for row in studentData:
         print(f""" First Name:{row[0]}\n Last Name:{row[1]}\n {row[2]}\n 
         MATH------>{row[3]}%\n 
@@ -125,19 +132,28 @@ def list_student():
         HISTORY--->{row[5]}%\n 
         GYM------->{row[6]}%\n 
         STUDENT AVERAGE--->{row[7]}%\n----------------------------""")
-        back = input("Press any key to go back to menu...")
+    back = input("Press the enter key to go back to menu...")
+    os.system('cls')
     main()
-
-
-def student_average(student_data):
-
-    print("student average")
     
 
 
 def course_average():
-    print("course average")
-    # can be hardcoded
+    courses = ["Math", "English", "History", "Gym"]
+    for i in courses:
+            x = 0
+            for student in studentData:
+                x += student[courses.index(i) + 3]      
+            print (f"{i} avg: {x / studentData}") 
+            #len(studentData)
+    back = input("Press any key to go back to menu...")
+    main()
 
+def quit():
+    os.system('cls')    
+    print("Application Shutting Down...")
+    time.sleep(2)
+    os.system('cls')    
+    print("Shut down")
 
 main()
